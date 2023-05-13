33ee3f0f8ad6112527a67e55f6b7e671df78b619
scottascott <scottwangg@gmail.com>
2023-05-12 22:12:51 -0400

    team

--- src/pages/components/characterShow.tsx
+++ src/pages/components/characterShow.tsx
@@ -160,6 +160,64 @@ const CharacterShow = (props: ChooseProps) => {
         }
         return <>{t("Currently No Data")}</>;
     }, [info, t]);
+    const [teamState, setTeamState] = useState<number>(-1);
+    const teamShow = useMemo(() => {
+        if (info && info.priority) {
+            const r = info.team;
+            if (r && r.length > 0) {
+                return (
+                    <>
+                        <div
+                            className="title bg-gradient-to-r from-[#ffcd29] flex ml-[10px] py-[10px] sm:py-[20px] pl-[20px] pr-[60px] w-fit rounded-l-[20px] items-center mt-[40px]"
+                            id="team"
+                        >
+                            <h1 className="inline-block !mr-[20px] text-[16px] sm:text-[20px]">
+                                {t("Team")}
+                            </h1>
+                        </div>
+                        <Divider />
+                        <div className="flex flex-col  px-[20px]">
+                            {r.map((p: TeamMemberProps, index) => {
+                                return (
+                                    // card
+                                    <div
+                                        key={index}
+                                        className="relative mt-[80px] rounded-[20px] px-[20px] py-[30px] shadow-md w-full h-fit sm:min-h-[160px] outline outline-1 outline-[#ffcd29]"
+                                    >
+                                        {/* element icon */}
+                                        <img
+                                            className="w-[40px] absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2"
+                                            src={p.icon}
+                                            alt=""
+                                        ></img>
+                                        {/* image & name */}
+                                        <div className="absolute text-center left-1/2 top-0 -translate-x-1/2 -translate-y-1/2 sm:left-[60px] sm:top-1/2 sm:-translate-y-1/2">
+                                            <img
+                                                className={`${
+                                                    teamState == index ? "sepia-0" : "sepia"
+                                                } w-[64px] mx-auto mb-[8px] sm:mb-[16px] cursor-pointer`}
+                                                src={p.img}
+                                                alt=""
+                                                onMouseEnter={() => setTeamState(index)}
+                                                onMouseLeave={() => setTeamState(-1)}
+                                                onClick={() => props.setC(p.name)}
+                                            ></img>
+                                            <h3>{t(p.name)}</h3>
+                                        </div>
+                                        {/* des */}
+                                        <div className="pt-[30px] sm:pt-0 sm:pl-[160px]">
+                                            <p>{t(p.des)}</p>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </>
+                );
+            }
+        }
+        return <></>;
+    }, [info, t, teamState]);
     return info ? (
         <motion.div
             initial={{ opacity: 0, y: 20 }}
@@ -176,7 +234,7 @@ const CharacterShow = (props: ChooseProps) => {
             <Divider />
             <div className="flex flex-wrap justify-center sm:justify-between">
                 <img
-                    className="-skew-y-6 drop-shadow-2xl w-[200px] h-fit sm:mt-[30px]"
+                    className="-skew-y-6 drop-shadow-2xl w-[200px] h-fit sm:mt-[30px] ml-0 sm:ml-[30px]"
                     src={info.img}
                     alt=""
                 ></img>
@@ -228,14 +286,7 @@ const CharacterShow = (props: ChooseProps) => {
                 </div>
             </div>
             {/* teamMates */}
-            <div
-                className="title bg-gradient-to-r from-[#ffcd29] flex ml-[10px] py-[10px] sm:py-[20px] pl-[20px] pr-[60px] w-fit rounded-l-[20px] items-center mt-[40px]"
-                id="team"
-            >
-                <h1 className="inline-block !mr-[20px] text-[16px] sm:text-[20px]">{t("Team")}</h1>
-            </div>
-            <Divider />
-            <div className="flex flex-wrap justify-center sm:justify-between"></div>
+            {teamShow}
         </motion.div>
     ) : (
         <></>
